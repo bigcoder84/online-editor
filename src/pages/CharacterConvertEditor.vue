f3db7559b45ad0d0c10d128f98a571bd692ff930
tianjindong <tianjindong98@qq.com>
2020-11-13 21:56:34 +0800

    完成富文本编辑转HTML基本功能

---
+++ src/pages/CharacterConvertEditor.vue
@@ -0,0 +1,44 @@
+<template>
+  <div style="width: 80%;margin-left: auto;margin-right: auto;">
+    <h1>富文本转HTML工具</h1>
+    <div id="editor">
+      <h3>欢迎使用富文本转HTML工具</h3>
+    </div>
+    <div>
+      ||
+    </div>
+    <textarea id="text" style="width:99%; height: 500px"></textarea>
+  </div>
+</template>
+
+<script>
+import Editor from 'wangeditor'
+import xmlFomatter from "@/utils/xmlFomatter";
+
+export default {
+  name: "editor",
+  data() {
+    return {
+      content: ""
+    }
+  },
+  methods: {},
+  mounted() {
+    const editor = new Editor('#editor');
+    //高度
+    editor.config.height = 500
+    editor.config.onchange = (html) => {
+      // 第二步，监控变化，同步更新到 textarea
+      let xml = xmlFomatter(html);
+      document.getElementById("text").value = xml;
+      this.content = xml;
+    }
+    editor.create();
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,23 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
+import CharacterConvertEditor from "@/pages/CharacterConvertEditor";
 
 Vue.use(VueRouter);
 
 
 const vueRouter = new VueRouter({
     //配置路径和组件的关系
-    routes:[
-
-    ]
+    routes: [
+        {
+            path: "",
+            redirect: "/characterConvertEditor"
+        },
+        {
+            path: "/characterConvertEditor",
+            component: CharacterConvertEditor
+        },
+    ],
+    mode: 'history'
 });
 
 export default vueRouter;
