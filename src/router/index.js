bf9b515528e09bb94730ab5bcef87a7a312d0d47
tianjindong <tianjindong98@qq.com>
2020-11-13 20:33:59 +0800

    add wangeditor、vuerouter

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+Vue.use(VueRouter);
+
+
+const vueRouter = new VueRouter({
+    //配置路径和组件的关系
+    routes:[
+
+    ]
+});
+
+export default vueRouter;
